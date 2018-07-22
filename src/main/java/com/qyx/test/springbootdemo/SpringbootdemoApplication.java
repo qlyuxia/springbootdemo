202875d71e48ad55c2f3cd5d89ee373c6dcd6381
qyx <1438633020@qq.com>
2018-07-22 22:11:41 +0800

    test springboot

---
+++ src/main/java/com/qyx/test/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,44 @@
+package com.qyx.test.springbootdemo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+@RequestMapping("test")
+public class SpringbootdemoApplication {
+
+	@RequestMapping("hello")
+	@ResponseBody
+	public String test(){
+		return "Hello World!";
+	}
+
+	@Bean
+	public My my(){
+		return new My();
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootdemoApplication.class, args);
+		applicationContext.getBean(My.class).test();
+	}
+
+	static class My implements InitializingBean {
+		@Override
+		public void afterPropertiesSet() throws Exception {
+			System.out.println("test bean");
+		}
+
+		public void test(){
+			System.out.println("haha");
+		}
+	}
+}
